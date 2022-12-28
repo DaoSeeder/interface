3ba66b3915b143f234873192f0118065589242bf
UsamaSaleem14 <usamasaleem0148@gmail.com>
2022-12-28 18:13:06 +0500

    Landing page changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,46 @@
 import React from "react";
 import "./App.css";
-import videoBg from "./assets/Galaxy2.mp4";
+import videoBg from "./assets/Galaxy.mp4";
 import Facebook from "./assets/facebook.png";
-import Instagram from "./assets/instagram.png";
+// import Instagram from "./assets/instagram.png";
 import Twitter from "./assets/twitter.png";
-import Telegram from "./assets/telegram.png";
+// import Telegram from "./assets/telegram.png";
+import logo from "./assets/logo2.png";
 
 function App() {
   return (
     <div className="App">
       <video src={videoBg} autoPlay muted loop />
       <div className="content">
-        <h1>DaoSeeder</h1>
-        <p>A decentalized crowdfunding platform</p>
-        <button>Get notified at launch</button>
+        <div className="brand">
+          <img src={logo} width={100} />
+          <h1>DaoSeeder</h1>
+        </div>
+        <p>A decentralized crowd-funding platform with trust</p>
+        <button
+          onClick={() => {
+            window.open("https://form.typeform.com/to/GOOMPc2I");
+          }}
+        >
+          Get notified at launch
+        </button>
         <div className="icons">
-          <img src={Facebook} />
-          <img src={Instagram} />
-          <img src={Twitter} />
-          <img src={Telegram} />
+          <img
+            src={Facebook}
+            onClick={() => {
+              window.open(
+                "https://www.facebook.com/profile.php?id=100088729073591"
+              );
+            }}
+          />
+          {/* <img src={Instagram} /> */}
+          <img
+            src={Twitter}
+            onClick={() => {
+              window.open("https://twitter.com/DaoSeederLabs");
+            }}
+          />
+          {/* <img src={Telegram} /> */}
         </div>
       </div>
     </div>
