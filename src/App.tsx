1f1d946f41c1185c09b93b17590d4399e9f72295
UsamaSaleem14 <usamasaleem0148@gmail.com>
2022-12-29 22:25:55 +0500

    Font + Button + Social Icon changes

--- src/App.tsx
+++ src/App.tsx
@@ -9,20 +9,27 @@ import logo from "./assets/logo2.png";
 function App() {
   return (
     <div className="App">
+      <div className="overlay"></div>
       <video src={videoBg} autoPlay muted loop />
       <div className="content">
         <div className="brand">
           <img src={logo} width={100} />
           <h1>DaoSeeder</h1>
         </div>
-        <p>A decentralized crowd-funding platform with trust</p>
-        <button
-          onClick={() => {
-            window.open("https://form.typeform.com/to/GOOMPc2I");
-          }}
+        <p>
+          Are you are crypto investor? Looking for cool crypto projects you can
+          be a part of. Be confident in projects by their project details and
+          their historical deliverables. You become part of the DAO to judge
+          their successes and achievements. Be a part of the web3 revolution.
+        </p>
+        <a
+          className="btnForm"
+          href="https://form.typeform.com/to/GOOMPc2I"
+          target={"_blank"}
+          rel="noreferrer"
         >
           Get notified at launch
-        </button>
+        </a>
         <div className="icons">
           <img
             src={Facebook}
@@ -32,15 +39,20 @@ function App() {
               );
             }}
           />
+          <hr className="line" />
           <img
             src={Twitter}
             onClick={() => {
               window.open("https://twitter.com/DaoSeederLabs");
             }}
           />
-          <a href="mailto:team@daoseeder.com">
-            <img src={Email} />
-          </a>
+          <hr className="line" />
+          <img
+            src={Email}
+            onClick={() => {
+              window.location.href = "mailto:team@daoseeder.com";
+            }}
+          />
         </div>
       </div>
     </div>
