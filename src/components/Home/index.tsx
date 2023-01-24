0fed9456e0dd01d4878110a589905e33c9104681
UsamaSaleem14 <usamasaleem0148@gmail.com>
2023-01-24 13:31:42 +0500

    Dark theme changes

--- src/components/Home/index.tsx
+++ src/components/Home/index.tsx
@@ -6,33 +6,35 @@ import Image3 from "../../assets/image_3.png";
 
 const Home = () => {
   const style = {
-    learnMoreBtnDiv: "text-font-lightV1",
+    learnMoreBtnDiv: "text-light-font-lightV1",
     btnLearnMore:
-      "cursor-pointer w-fit rounded-full bg-gradient-to-r from-primary-primary to-primary-secondary p-[2px]",
+      "cursor-pointer w-fit rounded-full bg-gradient-to-r from-light-primary-primary to-light-primary-secondary p-[2px]",
     btnLearnMoreContainer:
-      "flex h-full items-center justify-center bg-white back rounded-full px-10 py-2 font-bold",
+      "flex h-full items-center justify-center bg-white dark:bg-dark-background dark:text-dark-font-lightV1 rounded-full px-10 py-2 font-bold",
     banner:
       "flex flex-col justify-center items-center h-[550px] text-center relative",
     bannerHeading:
-      "text-font-lightV1 font-bold text-[66px] leading-[90px] mb-8",
-    bannerImg: "absolute z-[-1] right-[-10rem] w-[600px] top-[9rem]",
+      "text-light-font-lightV1 dark:text-dark-font-lightV1 font-bold text-[66px] leading-[90px] mb-8 z-[10]",
+    bannerImg:
+      "absolute z-[0] right-[-10rem] w-[600px] top-[9rem] dark:opacity-80",
     campaignsDiv: "flex flex-col mt-24",
-    campaignHeading: "text-center text-3xl text-font-lightV1 font-bold",
+    campaignHeading:
+      "text-center text-3xl text-light-font-lightV1 dark:text-dark-font-lightV1 font-bold",
     campaignCategories: "flex items-center mt-12 justify-between",
-    browse: "font-bold",
-    categoriesBtnDiv: "text-font-lightV1",
+    browse: "font-bold dark:text-dark-font-lightV1",
+    categoriesBtnDiv: "text-light-font-lightV1",
     btnCategories:
-      "cursor-pointer w-fit rounded-full bg-gradient-to-r from-primary-primary to-primary-secondary p-[2px]",
+      "cursor-pointer w-fit rounded-full bg-gradient-to-r from-light-primary-primary to-light-primary-secondary p-[2px]",
     btnCategoriesContainer:
-      "flex h-full items-center justify-center bg-white back rounded-full px-6 py-2",
+      "flex h-full items-center justify-center bg-white dark:bg-dark-background dark:text-dark-font-lightV1 rounded-full px-6 py-2",
     btnCategoriesContainerActive:
-      "flex h-full items-center justify-center text-font-lightV2 back rounded-full px-6 py-2",
+      "flex h-full items-center justify-center text-light-font-lightV2 back rounded-full px-6 py-2",
     allCampaigns: "my-12",
-    campaignDiv: "text-font-lightV1",
+    campaignDiv: "text-light-font-lightV1 dark:text-dark-font-lightV1",
     signleCampaign:
-      "cursor-pointer w-fit rounded-md bg-gradient-to-r from-white to-white p-[2px] drop-shadow-xl",
+      "cursor-pointer w-fit rounded-md bg-gradient-to-r from-white to-white dark:from-transparent dark:to-transparent p-[2px] drop-shadow-xl",
     signleCampaignContainer:
-      "flex flex-col h-full items-center justify-center bg-gradient-to-b from-[#9A9A9A]/20 to-[#9A9A9A]/10 back rounded-md px-6 py-2",
+      "flex flex-col h-full items-center justify-center bg-gradient-to-b from-[#9A9A9A]/20 to-[#9A9A9A]/10 dark:from-dark-box dark:to-dark-box rounded-md px-6 py-2",
     campaignName: "flex flex-row justify-between mt-4 w-full items-center",
     campaignTitle: "font-bold",
     campaignCategory: "text-xs",
@@ -146,7 +148,7 @@ const Home = () => {
               <div className={style.campaingDesc}>
                 <p>
                   This is a banking application where the user can manage their
-                  funds and can ...
+                  funds ...
                 </p>
               </div>
               <div className={style.campaignData}>
