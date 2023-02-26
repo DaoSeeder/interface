3e94edac877ebea2252fbc6434710881e1eff174
TBomberman <godwinwoo@gmail.com>
2023-02-26 12:25:50 -0800

    Start seed.

--- scripts/startGanache.sh
+++ scripts/startGanache.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-ganache --database.dbPath="../ganache/" --chain.chainId=326 --wallet.defaultBalance=10000 --chain.allowUnlimitedContractSize --miner.blockGasLimit="0x989680" --miner.timestampIncrement=180 --miner.defaultGasPrice="0x1DCD65000"
+ganache -s 326 --database.dbPath="../ganache/" --chain.chainId=326 --wallet.defaultBalance=10000 --chain.allowUnlimitedContractSize --miner.blockGasLimit="0x989680" --miner.timestampIncrement=180 --miner.defaultGasPrice="0x1DCD65000"
