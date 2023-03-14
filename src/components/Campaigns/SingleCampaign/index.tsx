d749100d93703e535f9f443996eab2edc06e4219
TBomberman <godwinwoo@gmail.com>
2023-03-14 13:01:28 -0700

    Move title to top.

--- src/components/Campaigns/SingleCampaign/index.tsx
+++ src/components/Campaigns/SingleCampaign/index.tsx
@@ -12,7 +12,7 @@ function SingleCampaign() {
     useSingleCampaignHandler();
   const style = {
     campaignDiv:
-      "text-light-font-lightV1 dark:text-dark-font-lightV1 mt-12 w-full",
+      "text-light-font-lightV1 dark:text-dark-font-lightV1 mt-4 w-full",
     mainCampaign:
       "rounded-md bg-gradient-to-r from-white to-white dark:from-transparent dark:to-transparent p-[2px] w-full drop-shadow-xl",
     singleCampaign:
@@ -22,7 +22,7 @@ function SingleCampaign() {
     singleCampaignContainer:
       "flex flex-col w-full h-full items-center justify-center bg-gradient-to-b from-[#9A9A9A]/20 to-[#9A9A9A]/10 dark:from-dark-box dark:to-dark-box rounded-md px-6 py-2",
     campaignName: "flex flex-row justify-between mt-4 w-full items-center",
-    campaignTitle: "font-bold",
+    campaignTitle: "font-bold mt-4",
     campaignCategory: "text-xs",
     campaignMoney: "text-sm",
     allCampaigns: "mb-12",
@@ -35,7 +35,7 @@ function SingleCampaign() {
     carouselArrows: "flex justify-center items-center gap-4 mt-8 select-none",
     carouselLeft:
       "cursor-pointer w-fit rounded-full bg-gradient-to-r from-light-primary-primary to-light-primary-secondary py-1 px-3 text-light-font-lightV2 font-bold text-lg",
-    campaignDetails:
+    campaignHeader:
       "font-bold text-2xl text-light-font-lightV1 dark:text-dark-font-lightV1 mt-12",
     allStages: "flex items-center",
     stageDetails:
@@ -56,6 +56,9 @@ function SingleCampaign() {
   };
   return (
     <>
+      <div className={style.campaignHeader}>
+        <p>{campaign?.name}</p>
+      </div>
       <div className={style.campaignDiv}>
         <div className={style.mainCampaign}>
           <div className={style.campaignImageContainer}>
@@ -90,12 +93,6 @@ function SingleCampaign() {
           {">"}
         </div>
       </div>
-      <div className={style.campaignDetails}>
-        <p>Campaign Details</p>
-      </div>
-      <div className={style.campaignWebsite}>
-        <p>{campaign?.name}</p>
-      </div>
       <div className={style.campaignWebsite}>
         <p>{campaign?.description}</p>
       </div>
@@ -109,7 +106,7 @@ function SingleCampaign() {
           }}
         />
       </div>
-      <div className={style.campaignDetails}>
+      <div className={style.campaignTitle}>
         <p>Token Address</p>
       </div>
       <div className={style.campaignWebsite}>
