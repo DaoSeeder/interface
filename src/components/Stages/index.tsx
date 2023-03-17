f4e21848c5ea253f9933bacfa681a32e583eadf6
TBomberman <godwinwoo@hotmail.com>
2023-03-16 18:14:19 -0700

    Added vote end.

--- src/components/Stages/index.tsx
+++ src/components/Stages/index.tsx
@@ -38,6 +38,7 @@ function Stage() {
     showVotingBtn,
     currBlock,
     expiryDate,
+    voteEndDate,
     currBlockTime,
     showCommitBtn,
   } = useSingleStageHandler();
@@ -137,8 +138,12 @@ function Stage() {
                   </div>
                   <div className={style.blockTime}>
                     <div className={style.totalTimeLeft}>
-                      Expiration block: {stageData?.stageContract.expiryBlock}{" "}
+                      Expiration Block: {stageData?.stageContract.expiryBlock}{" "}
                       <span className={style.expDate}>({expiryDate})</span>
+                    </div>
+                    <div className={style.totalTimeLeft}>
+                      Vote End Block: {stageData?.stageContract.voteEndBlock}{" "}
+                      <span className={style.expDate}>({voteEndDate})</span>
                     </div>
                     <div className={style.totalTimeLeft}>
                       Current Block: {currBlock}{" "}
