e680f090bcfc6bae106c07f8a59be27c970a3ee0
UsamaSaleem14 <usamasaleem0148@gmail.com>
2023-03-31 21:29:19 +0500

    Added video in popup

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,68 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
-import videoBg from "./assets/daoseeder.mp4";
+import videoBg from "./assets/Galaxy.mp4";
 import Facebook from "./assets/facebook.png";
 import Twitter from "./assets/twitter.png";
 import Email from "./assets/email.png";
 import logo from "./assets/logo2.png";
+import PlayBtn from "./assets/playicon.png";
+import Modal from "react-modal";
+import DaoSeeder from "./assets/daoseeder.mp4";
+import Cross from "./assets/cross.png";
 
 function App() {
+  const [modalIsOpen, setModalIsOpen] = useState(false);
+
+  function openModal() {
+    setModalIsOpen(true);
+  }
+
+  function closeModal() {
+    setModalIsOpen(false);
+  }
+
   return (
     <div className="App">
+      <Modal
+        isOpen={modalIsOpen}
+        onRequestClose={closeModal}
+        contentLabel="Video Popup"
+      >
+        <div className="daoseederVideo">
+          <button onClick={closeModal} className="modalBtn">
+            <img src={Cross} alt="cross" />
+          </button>
+          <video
+            controls
+            autoPlay
+            style={{ maxWidth: "80%", maxHeight: "80%" }}
+          >
+            <source src={DaoSeeder} type="video/mp4" />
+          </video>
+        </div>
+      </Modal>
       <div className="overlay"></div>
-      {/* <video src={videoBg} autoPlay muted loop /> */}
+      <video src={videoBg} autoPlay muted loop />
       <div className="content">
         <div className="brand">
-          <img src={logo} width={60} />
+          <img src={logo} width={100} />
           <h1>DaoSeeder</h1>
         </div>
         <p>A decentralized crowd-funding platform with trust</p>
-        <div>
-          <a
-            className="btnForm"
-            href="https://app.daoseeder.com"
-            target={"_blank"}
-            rel="noreferrer"
-          >
-            Launch App
-          </a>
-        </div>
-        <video src={videoBg} autoPlay muted loop />
+        <img
+          src={PlayBtn}
+          alt="playbtn"
+          className="playBtn"
+          onClick={openModal}
+        />
+        <a
+          className="btnForm"
+          href="https://app.daoseeder.com"
+          target={"_blank"}
+          rel="noreferrer"
+        >
+          Launch App
+        </a>
         <div className="icons">
           <img
             src={Facebook}
