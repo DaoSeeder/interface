f0ab44d1e25672abdf57ed97cb0df3490accf9c5
UsamaSaleem14 <usamasaleem0148@gmail.com>
2023-04-01 12:49:48 +0500

    Comment changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, Fragment, useRef } from "react";
 import "./App.css";
 import videoBg from "./assets/Galaxy.mp4";
 import Facebook from "./assets/facebook.png";
@@ -6,12 +6,12 @@ import Twitter from "./assets/twitter.png";
 import Email from "./assets/email.png";
 import logo from "./assets/logo2.png";
 import PlayBtn from "./assets/playicon.png";
-import Modal from "react-modal";
 import DaoSeeder from "./assets/daoseeder.mp4";
-import Cross from "./assets/cross.png";
+import { Dialog, Transition } from "@headlessui/react";
 
 function App() {
   const [modalIsOpen, setModalIsOpen] = useState(false);
+  const [isPlaying, setIsPlaying] = useState(false);
 
   function openModal() {
     setModalIsOpen(true);
@@ -21,26 +21,74 @@ function App() {
     setModalIsOpen(false);
   }
 
+  const videoRef = useRef<HTMLVideoElement>(null);
+
+  const handlePlayButtonClick = () => {
+    if (!videoRef || !videoRef.current) return;
+    setIsPlaying(true);
+    videoRef.current.play();
+  };
+
   return (
     <div className="App">
-      <Modal
-        isOpen={modalIsOpen}
-        onRequestClose={closeModal}
-        contentLabel="Video Popup"
-      >
-        <div className="daoseederVideo">
-          <button onClick={closeModal} className="modalBtn">
-            <img src={Cross} alt="cross" />
-          </button>
-          <video
-            controls
-            autoPlay
-            style={{ maxWidth: "80%", maxHeight: "80%" }}
+      {/* Modal */}
+      <Transition appear show={modalIsOpen} as={Fragment}>
+        <Dialog as="div" onClose={closeModal} className={"dialog"}>
+          <Transition.Child
+            as={Fragment}
+            enter="ease-out duration-300"
+            enterFrom="opacity-0"
+            enterTo="opacity-100"
+            leave="ease-in duration-200"
+            leaveFrom="opacity-100"
+            leaveTo="opacity-0"
           >
-            <source src={DaoSeeder} type="video/mp4" />
-          </video>
-        </div>
-      </Modal>
+            <div className={"overlay"}></div>
+          </Transition.Child>
+          <div className={"dialogMain"}>
+            <div className={"dialogDiv"}>
+              <Transition.Child
+                as={Fragment}
+                enter="ease-out duration-300"
+                enterFrom="opacity-0 scale-95"
+                enterTo="opacity-100 scale-100"
+                leave="ease-in duration-200"
+                leaveFrom="opacity-100 scale-100"
+                leaveTo="opacity-0 scale-95"
+              >
+                <Dialog.Panel className={"dialogPanel"}>
+                  <div className={"dialogBody"}>
+                    <div className="video-container">
+                      <video
+                        onPlay={handlePlayButtonClick}
+                        onPause={() => setIsPlaying(false)}
+                        ref={videoRef}
+                        controls
+                        autoPlay={false}
+                      >
+                        <source src={DaoSeeder} type="video/mp4" />
+                      </video>
+                      {!isPlaying && (
+                        <div
+                          className="play-button"
+                          onClick={handlePlayButtonClick}
+                        >
+                          <img
+                            src={PlayBtn}
+                            alt="playbtn"
+                            className="playBtn"
+                          />
+                        </div>
+                      )}
+                    </div>
+                  </div>
+                </Dialog.Panel>
+              </Transition.Child>
+            </div>
+          </div>
+        </Dialog>
+      </Transition>
+      {/* Modal */}
       <div className="overlay"></div>
       <video src={videoBg} autoPlay muted loop />
       <div className="content">
@@ -61,7 +109,7 @@ function App() {
           target={"_blank"}
           rel="noreferrer"
         >
-          Launch App
+          Launch Test App
         </a>
         <div className="icons">
           <img
