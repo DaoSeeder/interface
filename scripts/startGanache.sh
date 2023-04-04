61e69f4515225add4f090405d4970cfc6104a055
TBomberman <godwinwoo@hotmail.com>
2023-04-03 23:41:03 -0700

    Sync ganach block gas limit with hardhat.

--- scripts/startGanache.sh
+++ scripts/startGanache.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-ganache -s 326 --database.dbPath="../ganache/" --chain.chainId=326 --wallet.defaultBalance=10000 --chain.allowUnlimitedContractSize --miner.blockGasLimit="0x989680" --miner.blockTime=10 --miner.defaultGasPrice="0x1DCD65000"
+ganache -s 326 --database.dbPath="../ganache/" --chain.chainId=326 --wallet.defaultBalance=10000 --chain.allowUnlimitedContractSize --miner.blockGasLimit="0x1C9C380" --miner.blockTime=10 --miner.defaultGasPrice="0x1DCD65000"
